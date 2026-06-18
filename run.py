f7aa63ff76218d6e0c0f9ebc4e7aae6c31dafbde
jaisveenkaur <jaisveenkaur15@gmail.com>
2026-06-18 17:03:37 +0530

    configure run.py for Render deployment compatibility

--- run.py
+++ run.py
@@ -11,6 +11,6 @@ env = os.environ.get('FLASK_ENV', 'development')
 app = create_app(env)
 
 if __name__ == '__main__':
-    host = os.environ.get('FLASK_RUN_HOST', '127.0.0.1')
-    port = int(os.environ.get('FLASK_RUN_PORT', 5001))
+    host = os.environ.get('FLASK_RUN_HOST', '0.0.0.0')
+    port = int(os.environ.get('PORT', 5001))
     app.run(host=host, port=port)
